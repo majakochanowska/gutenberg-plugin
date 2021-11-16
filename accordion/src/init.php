5f4c1af01a53ce4b160b247cb80f6984dff1d1da
Maja Kochanowska <maja.kochanowska@mayeryn.com>
2021-11-16 22:55:12 +0100

    frontend script added and package name changed

--- accordion/src/init.php
+++ accordion/src/init.php
@@ -5,7 +5,7 @@
  * Enqueue CSS/JS of all the blocks.
  *
  * @since   1.0.0
- * @package CGB
+ * @package gutenberg
  */
 
 // Exit if accessed directly.
@@ -27,10 +27,10 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @uses {wp-editor} for WP editor styles.
  * @since 1.0.0
  */
-function accordion_cgb_block_assets() { // phpcs:ignore
+function accordion_gutenberg_block_assets() { // phpcs:ignore
 	// Register block styles for both frontend + backend.
 	wp_register_style(
-		'accordion-cgb-style-css', // Handle.
+		'accordion-gutenberg-style-css', // Handle.
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
 		is_admin() ? array( 'wp-editor' ) : null, // Dependency to include the CSS after it.
 		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
@@ -38,7 +38,7 @@ function accordion_cgb_block_assets() { // phpcs:ignore
 
 	// Register block editor script for backend.
 	wp_register_script(
-		'accordion-cgb-block-js', // Handle.
+		'accordion-gutenberg-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
 		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
@@ -47,7 +47,7 @@ function accordion_cgb_block_assets() { // phpcs:ignore
 
 	// Register block editor styles for backend.
 	wp_register_style(
-		'accordion-cgb-block-editor-css', // Handle.
+		'accordion-gutenberg-block-editor-css', // Handle.
 		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
 		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
 		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
@@ -55,7 +55,7 @@ function accordion_cgb_block_assets() { // phpcs:ignore
 
 	// WP Localized globals. Use dynamic PHP stuff in JavaScript via `cgbGlobal` object.
 	wp_localize_script(
-		'accordion-cgb-block-js',
+		'accordion-gutenberg-block-js',
 		'cgbGlobal', // Array containing dynamic data for a JS Global.
 		[
 			'pluginDirPath' => plugin_dir_path( __DIR__ ),
@@ -75,16 +75,23 @@ function accordion_cgb_block_assets() { // phpcs:ignore
 	 * @since 1.16.0
 	 */
 	register_block_type(
-		'cgb/block-accordion', array(
+		'gutenberg/accordion', array(
 			// Enqueue blocks.style.build.css on both frontend & backend.
-			'style'         => 'accordion-cgb-style-css',
+			'style'         => 'accordion-gutenberg-style-css',
 			// Enqueue blocks.build.js in the editor only.
-			'editor_script' => 'accordion-cgb-block-js',
+			'editor_script' => 'accordion-gutenberg-block-js',
 			// Enqueue blocks.editor.build.css in the editor only.
-			'editor_style'  => 'accordion-cgb-block-editor-css',
+			'editor_style'  => 'accordion-gutenberg-block-editor-css',
 		)
 	);
 }
 
+function enqueue_frontend_script() {
+
+	wp_enqueue_script( 'accordion-gutenberg-frontend-js', esc_url( plugins_url( 'scripts/frontend.js', dirname( __FILE__ ) ) ), array(), '1.0.0', true );
+}
+
 // Hook: Block assets.
-add_action( 'init', 'accordion_cgb_block_assets' );
+add_action( 'init', 'accordion_gutenberg_block_assets' );
+
+add_action( 'wp_enqueue_scripts', 'enqueue_frontend_script' );
